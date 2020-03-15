c972420c0bf29c3cf2129ba65062e4a8e7885179
Fr0ntender <kev987@ya.ru>
2020-03-15 21:07:33 +0500

    add

---
+++ config.py
@@ -0,0 +1,7 @@
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgres://ethers:kd0c1ciLq@localhost'
+    SECRET_KEY = 'iLq@ld0chos'
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'bcrypt'
